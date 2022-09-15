71c6601a958fce1c795e1287f7e7b0403277de69
techjunkie <tolaniolowosule@gmail.com>
2022-09-15 13:28:50 -0400

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,1 +1,35 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+  default = "udacity"
+}
 
+variable "environment"{
+  description = "The environment should be used for all resources in this example"
+  default = "test"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+  default = "US East" 
+}
+
+variable "username"{
+  default = "username"
+}
+
+variable "password"{
+  default= "password"
+}
+
+variable "server_names"{
+  type = list
+  default = ["uat","int"]
+}
+
+variable "packerImageId"{
+  default = "/subscriptions/59ce2236-a139-4c59-91d8-bedbf055dbb9/resourceGroups/udacity-rg/providers/Microsoft.Compute/images/PackerImage"
+}
+
+variable "vm_count"{
+  default = "2"
+}
